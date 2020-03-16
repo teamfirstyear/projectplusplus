bba12369fecbce68226ceb884e9b0316b74f9760
Hà Đạt <hatrongdat219@gmail.com>
2020-03-16 20:12:28 +0700

    remove themmoi5

--- thembuoi5/Qltc.java
+++
@@ -1,9 +0,0 @@
-package thembuoi5;
-
-public interface Qltc {
-    void themmoi();
-    void xoatailieu();
-    void hienthi();
-    void timkiemtailieu(String search1,String search2);
-    void them();
-}
